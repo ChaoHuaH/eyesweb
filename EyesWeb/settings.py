bc87a6768a68ab12951cab4a1463be60e7e42a5d
ChaoHua <chaohua77@outlook.com>
2021-01-05 12:21:24 +0800

    for heroku uploading

--- EyesWeb/settings.py
+++ EyesWeb/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn EyesWeb.wsgi --log-file -
