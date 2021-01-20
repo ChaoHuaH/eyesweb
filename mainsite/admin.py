b9c417d4c2e202835bc6f11582c5cc39a3198ba5
ChaoHua <chaohua77@outlook.com>
2021-01-20 15:18:12 +0800

    MVC structure

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, NewTable, Product
+
 
 # Register your models here.
 
@@ -7,3 +8,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display=('title', 'slug', 'pub_date')
 
 admin.site.register(Post, PostAdmin)
+
+admin.site.register(NewTable)
+
+admin.site.register(Product)
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from datetime import datetime
-from .models import Post
+from .models import Post, Product
+import random
 
 # Create your views here.
 def homepage(request):
@@ -16,3 +17,88 @@ def showpost(request, slug):
             return render(request, 'post.html', locals())
     except:
         return redirect('/')
+
+# def about(request):
+#     html = '''
+# <!DOCTYPE html>
+# <html>
+# <head><title>About Myself</title></head>
+# <body>
+# <h2>C.H Huang</h2>
+# <hr>
+# <p>
+# Hi, I am C.H. Huang.
+# </p>
+# </body>
+# </html>
+# '''
+#     return HttpResponse(html)
+
+def about(request):
+    quotes = ['S1',
+                'S2',
+                'S3',
+                'S4']
+    quote = random.choices(quotes)
+    return render(request, 'about.html', locals())
+
+
+
+def listing(request):
+    html = '''
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>List</title>
+</head>
+<body>
+<h2>Folowing is the list of saling phone</h2>
+<hr>
+<table width=400 border=1 bgcolor='#ccffcc'>
+{}
+</table>
+</body>
+</html>
+'''
+    products = Product.objects.all()
+    tags = '<tr><td>Name</td><td>Price</td></tr>'
+    for p in products:
+        tags = tags + '<tr><td>{}</td>'.format(p.name)
+        tags = tags + '<td>{}</td>'.format(p.price)
+
+    return HttpResponse(html.format(tags))
+
+def disp_detail(request, sku):
+    html = '''
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = 'utf-8'>
+<title>{}</title>
+</head>
+<body>
+<h2>{}</h2>
+<hr>
+<table width=400 border=1 bgcolor='#ccffcc'>
+{}
+</table>
+<a href='/list'>Go back to List</a>
+</body>
+</html>
+'''
+    try:
+        p = Product.objects.get(sku=sku)
+    except Product.DoesNotExist:
+        raise Http404("Can't find the item number")
+    tags = '<tr><td>Item number</td><td>{}</td></tr>'.format(p.sku)
+    tags = tags + '<tr><td>Item name</td><td>{}</td><tr>'.format(p.name)
+    tags = tags + '<tr><td>Price</td><td>{}</td></tr>'.format(p.price)
+    return HttpResponse(html.format(p.name, p.name, tags))
+
+# def disp_detail(request, sku):
+#     try:
+#         p = Product.objects.all(sku=sku)
+#     except Product.DoesNotExist:
+#         raise Http404("Can't find the item")
+#     return render(request, 'disp.html', locals())
